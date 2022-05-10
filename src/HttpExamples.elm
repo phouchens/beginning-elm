c1d5db1fa3813826a786b5bc766f89b5511aad79
prh <prh@pop-os.localdomain>
2022-05-09 21:45:31 -0400

    updates http

--- src/HttpExamples.elm
+++ src/HttpExamples.elm
@@ -4,10 +4,13 @@ import Browser
 import Html exposing (..)
 import Html.Events exposing (onClick)
 import Http
+import RandomNumber exposing (init)
 
 
 type alias Model =
-    List String
+    { nickNames : List String
+    , errorMessage : Maybe String
+    }
 
 
 view : Model -> Html Msg
@@ -15,8 +18,37 @@ view model =
     div []
         [ button [ onClick SendHttpRequest ]
             [ text "Get data from server" ]
-        , h3 [] [ text "Old School main characters" ]
-        , ul [] (List.map viewNickname model)
+        , viewNicknamesOrError model
+        ]
+
+
+viewNicknamesOrError : Model -> Html Msg
+viewNicknamesOrError model =
+    case model.errorMessage of
+        Just message ->
+            viewError message
+
+        Nothing ->
+            viewNicknames model.nickNames
+
+
+viewError : String -> Html Msg
+viewError errorMessage =
+    let
+        errorHeadding =
+            "Couln't fetch nicknames at this time."
+    in
+    div []
+        [ h3 [] [ text errorHeadding ]
+        , text ("Error: " ++ errorMessage)
+        ]
+
+
+viewNicknames : List String -> Html Msg
+viewNicknames nicknames =
+    div []
+        [ h3 [] [ text "Old School Main characters" ]
+        , ul [] (List.map viewNickname nicknames)
         ]
 
 
@@ -32,7 +64,7 @@ type Msg
 
 url : String
 url =
-    "http://localhost:5016/old-school.txt"
+    "http://localhost:5016/old-school.txte"
 
 
 getNicknames : Cmd Msg
@@ -54,16 +86,44 @@ update msg model =
                 nicknames =
                     String.split "," nicknamesStr
             in
-            ( nicknames, Cmd.none )
+            ( { model | nickNames = nicknames }, Cmd.none )
 
-        DataReceived (Err _) ->
-            ( model, Cmd.none )
+        DataReceived (Err httpError) ->
+            ( { model
+                | errorMessage = Just (buildErrorMessage httpError)
+              }
+            , Cmd.none
+            )
+
+
+buildErrorMessage : Http.Error -> String
+buildErrorMessage httpError =
+    case httpError of
+        Http.BadUrl message ->
+            message
+
+        Http.Timeout ->
+            "Server is taking to long bro."
+
+        Http.NetworkError ->
+            "Can't reach the server man"
+
+        Http.BadStatus statusCode ->
+            "Request failed with the status code: " ++ String.fromInt statusCode
+
+        Http.BadBody message ->
+            message
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { nickNames = [], errorMessage = Nothing }, Cmd.none )
 
 
 main : Program () Model Msg
 main =
     Browser.element
-        { init = \_ -> ( [], Cmd.none )
+        { init = init
         , view = view
         , update = update
         , subscriptions = \_ -> Sub.none
